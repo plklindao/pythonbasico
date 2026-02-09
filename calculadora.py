a6b2105fe2b0bb638e3b7cd1f192131b38dcab87
Adryan <adryan.campos@escola.pr.gov.br>
2026-02-09 14:11:10 +0000

    s

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro número"))
+num2 = int(input("Digite o segundo número"))
+
+soma = num1 + num2
+divisao = num1 | num2
+multiplicação = num1 * num2
+subtracao = num1 - num2
+
+print("A soma é:",soma)
---
+++ ola.py
@@ -0,0 +1,9 @@
+#nome = input("Digite seu nome:")
+#print("Olá, ",nome ,"Seja bem vindo")
+
+
+
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+print("Olá, ",nome , sobrenome, "Seja bem vindo")
